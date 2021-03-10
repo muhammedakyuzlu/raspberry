633a3c8faaf559ccde5a89ac90079b3f2f7f78cb
Muhammed akyuzlu <muhammedakyuzu6@gmail.com>
2021-03-10 18:56:19 +0000

    distance sensor

--- ALL NEW/main.py
+++ ALL NEW/main.py
@@ -1,44 +1,32 @@
 from motor_module import Motors 
 import lane_detection_module
+from sensor import Sensor
 
-# # top left 
-# TLMotor = Motor(22,17,27)
-# # top right
-# TRMotor = Motor(14,15,18)
-# # buttom left
-# BLMotor = Motor(26,13,19)
-# # buttom right
-# BRMotor = Motor(16,20,21)
- 
-# MOTORS: ENA IN1 IN2 :top left  top right buttom left  buttom right
-motor = Motors(22,17,27,14,15,18,26,13,19,16,20,21) 
+
+
+
 
 
 def main():
+    distance = sensor.distance()
+    print ("Distance: %.1f cm" % distance)
 
-    motor.move(speed=1,turn=0.25,t=1)
-    motor.move(speed=1,turn=-0.25,t=1)
-    motor.move(speed=1,turn=1,t=1)
-    motor.move(speed=1,turn=-1,t=1)
-    motor.stop(1)
-    #pass
-    # #motor.move(Forward=True,speed=1,turn=0,t=3)
-    # #motor.stop(1)
-    # #motor.buttomMotors(speed=100,turn=0,t=0.1)
-    # #motor.topMotors(speed=100,turn=-90,t=3)
     
-    
-    # motor.move(speed=1,turn=0.25,t=1)
-    # motor.move(speed=1,turn=-0.25,t=1)
-    # motor.move(speed=1,turn=1,t=1)
-    # motor.move(speed=1,turn=-1,t=1)
-    # motor.stop(1)
-    # #motor.move(Forward=True,speed=1,turn=0,t=3)
-    # #motor.stop(1)
-    # #motor.move(Forward=False,speed=1,turn=0.5,t=3)
- 
+
+
 if __name__ == '__main__':
-    while True:
+
+    # MOTORS: ENA IN1 IN2 :top left  top right buttom left  buttom right
+    motor = Motors(22,17,27,14,15,18,26,13,19,16,20,21) 
+
+    # Sensor
+    sensor = Sensor(pinTrigger=23,pinEcho=24)
+
+
+
+    for i in range(5):
         main()
+    sensor.close()    
+
         
-#  GPIO.cleanup()        
+   
--- ALL NEW/sensor.py
+++ ALL NEW/sensor.py
@@ -6,44 +6,61 @@ import sys
 # use Raspberry Pi board pin numbers
 GPIO.setmode(GPIO.BCM)
 
-# set GPIO Pins
-pinTrigger = 23
-pinEcho = 24
 
-def close(signal, frame):
-	print("\nTurning off ultrasonic distance detection...\n")
-	GPIO.cleanup() 
-	sys.exit(0)
+class Sensor():
 
-signal.signal(signal.SIGINT, close)
+	def __init__(self,pinTrigger,pinEcho):
+		self.pinTrigger=pinTrigger
+		self.pinEcho=pinEcho
+		# set GPIO input and output channels
+		GPIO.setup(self.pinTrigger, GPIO.OUT)
+		GPIO.setup(self.pinEcho, GPIO.IN)
 
-# set GPIO input and output channels
-GPIO.setup(pinTrigger, GPIO.OUT)
-GPIO.setup(pinEcho, GPIO.IN)
 
-while True:
-	# set Trigger to HIGH
-	GPIO.output(pinTrigger, True)
-	# set Trigger after 0.01ms to LOW
-	time.sleep(0.00001)
-	GPIO.output(pinTrigger, False)
+	def distance(self):
+		#print("\nTurning on ultrasonic distance detection...\n")
+		# set Trigger to HIGH
+		GPIO.output(self.pinTrigger, True)
+		# set Trigger after 0.01ms to LOW
+		time.sleep(0.00001)
+		GPIO.output(self.pinTrigger, False)
 
-	startTime = time.time()
-	stopTime = time.time()
-
-	# save start time
-	while 0 == GPIO.input(pinEcho):
 		startTime = time.time()
-
-	# save time of arrival
-	while 1 == GPIO.input(pinEcho):
 		stopTime = time.time()
 
-	# time difference between start and arrival
-	TimeElapsed = stopTime - startTime
-	# multiply with the sonic speed (34300 cm/s)
-	# and divide by 2, because there and back
-	distance = (TimeElapsed * 34300) / 2
+		# save start time
+		while 0 == GPIO.input(self.pinEcho):
+			startTime = time.time()
 
-	print ("Distance: %.1f cm" % distance)
-	time.sleep(1)
+		# save time of arrival
+		while 1 == GPIO.input(self.pinEcho):
+			stopTime = time.time()
+
+		# time difference between start and arrival
+		TimeElapsed = stopTime - startTime
+		# multiply with the sonic speed (34300 cm/s)
+		# and divide by 2, because there and back
+		distance = (TimeElapsed * 34300) / 2
+
+		#print ("Distance: %.1f cm" % distance)
+		#time.sleep(1)
+		
+		return distance
+
+
+	def close(self):
+		#print("\nTurning off ultrasonic distance detection...\n")
+		GPIO.cleanup([self.pinTrigger,self.pinEcho]) 
+		#sys.exit(0)
+
+
+
+
+
+if __name__ == '__main__':	
+	while True:
+		dis = Sensor(23,24)
+		dis.distance()
+		dis.close()
+		time.sleep(2)
+        
