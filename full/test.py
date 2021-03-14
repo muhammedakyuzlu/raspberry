d12b3b7218520c06cdcb2aa39e109bebd435a7be
muhammed akyuzlu <muhammedakyuzlu6@gmail.com>
2021-03-15 02:41:08 +0300

    test file

--- full/test.py
+++ full/test.py
@@ -5,50 +5,36 @@ import lane_detection_module
 import camera
 import sys
 
-
-# the camera port 0 or 1
 CAMERA_PORT = 0
-# MOTORS: ENA IN1 IN2 :top left  top right buttom left  buttom right
-motor = Motors(22,17,27,5,6,12,26,13,19,16,20,21)
-# Sensor
-sensor = Sensor(pinTrigger=23,pinEcho=4)
-
 
 def main():
 
-    # get image
-    img = camera.getImg()
-
-    # if image got lost break
-    if img is None :
-            break
-
-    # get curve from image by detecting the lane lines
-    curve = lane_detection_module.getLaneCurve(img,display=0)
-    
-    print(curve)
-
     distance = sensor.distance()
-    
     print ("Distance: %.1f cm" % distance)
-
-    motor.move()
     
 
 
 if __name__ == '__main__':
+
+    # MOTORS: ENA IN1 IN2 :top left  top right buttom left  buttom right
+    #motor = Motors(22,17,27,5,6,12,26,13,19,16,20,21)
+    
+    # Sensor
+    sensor = Sensor(pinTrigger=23,pinEcho=4)
+
+
     try:
         while True:
             main()
     except KeyboardInterrupt:
         print('KeyboardInterrupt')
         sensor.close()
-        motor.stop()
+        #motor.stop()
         sys.exit(0)
     except Exception:
         print('something went wrong')
         sensor.close()
-        motor.stop()
+        #motor.stop()
         sys.exit(0)
            
 
